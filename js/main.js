19d55dd37bd4f0e561ca0be543f971c8e69cb8dc
davidpalacin <palacingimenezdavid.fpllefia@gmail.com>
2022-09-20 18:37:37 +0200

    comienzo javascript, añadir canales y usuarios a las listas laterales

---
+++ js/main.js
@@ -0,0 +1,24 @@
+let users = [];
+let channels = [];
+
+//AÑADIR NUEVO CANAL A LA LISTA
+$("#add-channel").click(func = () => {
+    let name_new_channel = prompt("nombre del canal nuevo");
+    if(name_new_channel != null && name_new_channel != ""){
+        channels.push(name_new_channel);
+        $("#channel_list").append(`<p># ${name_new_channel}</p>`);
+    }else{
+        alert("Operación cancelada");
+    }
+});
+
+//AÑADIR NUEVO USUARIO A LA LISTA
+$("#add-user").click(func = () => {
+    let name_new_user = prompt("nombre del canal nuevo");
+    if(name_new_user != null && name_new_user != ""){
+        users.push(name_new_user);
+        $("#user_list").append(`<p># ${name_new_user}</p>`);
+    }else{
+        alert("Operación cancelada");
+    }
+});
