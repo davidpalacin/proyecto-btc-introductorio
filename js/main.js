bcb0cbceba3199046d41b0d57290310e153328f8
davidpalacin <palacingimenezdavid.fpllefia@gmail.com>
2022-09-21 19:37:18 +0200

    javascript crear y chatear con un usuario mensajes directos

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 // Pasos:
-// 1. Al hacer click en añadir usuario crear una variable new User, y guardar objeto en el array de usuarios
+// 1. Al hacer click en añadir usuario, crear una variable new User
 // 2. Mostrar el nuevo usuario en la lista lateral de usuarios
 // 3. Al hacer click a un usuario entrar en una pantalla de mensajes directos, y los mensajes con este usuario
-const users = [];
-
 class User {
     constructor(name){
         this.name = name;
@@ -18,8 +16,6 @@ class User {
         for(let i=0; i<this.messages.length; i++){
             if(this.messages[i].from == from && this.messages[i].to == to){
                 result.push(this.messages[i]);
-            }else{
-                console.log("No hay mensajes entre estos usuarios");
             }
         }
         return result;
@@ -28,12 +24,10 @@ class User {
 let yourself = new User("Yo");
 
 $("#add-user").click(function (){
-    let new_name = prompt("Nombre del nuevo usuario");
+    let new_name = prompt("Mandarle un mensaje directo a: ");
     if(new_name != null && new_name != ""){
         let user = new User(new_name);
-        users.push(user);
-        $("#user_list").append(`<p># <span onclick="enterChat('${new_name}')">${new_name}</span></p>`);
-        console.log(users);
+        $("#user_list").append(`<p onclick="enterChat('${new_name}')"><i class="fa-solid fa-user"></i> ${new_name}</p>`);
     }else{
         alert("Información no válida");
     }
@@ -41,17 +35,24 @@ $("#add-user").click(function (){
 
 
 function enterChat(name){
+    $("#ipt-new-msg").focus();
     let to = name;
     $(".chat-title").html(`${to}`);
     $(".chat-descript").html(`Este es tu chat con ${to}`);
-    $(".msg-screen").html("");
     printMessages(to);
-    
 }
+
 function printMessages(to){
     $(".msg-screen").html("");
+
     let res = yourself.getMessage(yourself.name, to);
     if(res.length == 0){
+        let today = new Date();
+        let dd = String(today.getDate()).padStart(2, '0');
+        let mm = String(today.getMonth() + 1).padStart(2, '0');
+        let yyyy = today.getFullYear();
+        today = dd + '/' + mm + '/' + yyyy;
+
         $(".msg-screen").append(`
         <div class="msg"> 
         <div class="flex-msg">
@@ -59,7 +60,7 @@ function printMessages(to){
             <div class="details-msg">
                     <div class="flex-date-msg">
                         <p id="username-msg">Oops!</p>
-                        <span id="date-msg">--/--/--</span>
+                        <span id="date-msg">${today}</span>
                     </div>
                 <span id="content-msg">No tienes ningún mensaje con ${to}. ¡Dile Hola!</span>
             </div>
@@ -101,7 +102,7 @@ function sendMessage(destinatary){
 
     today = dd + '/' + mm + '/' + yyyy;
     let content = $("#ipt-new-msg").val();
-    yourself.setMessage(yourself.name, to, content, today)
+    yourself.setMessage(yourself.name, to, content, today);
     printMessages(to);
 
     $("#ipt-new-msg").val("");
