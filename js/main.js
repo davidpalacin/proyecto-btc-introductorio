e4e2c51768e7756f2668a0c1609e064855e9eea2
davidpalacin <palacingimenezdavid.fpllefia@gmail.com>
2022-09-22 16:24:50 +0200

    creacion canales y mostrar mensajes

--- js/main.js
+++ js/main.js
@@ -21,7 +21,43 @@ class User {
         return result;
     } 
 }
+class Channel {
+    constructor(name){
+        this.name = name;
+    }
+
+    channel_messages = [];
+    setMessage(from, to, content, date){
+        this.channel_messages.push({from: from, to: to, content: content, date: date});
+    }
+    getMessage(to){
+        let result = [];
+        for(let i=0; i<this.channel_messages.length; i++){
+            if(this.channel_messages[i].to == to){
+                result.push(this.channel_messages[i]);
+            }
+        }
+        return result;
+    } 
+}
+const users = [];
+const channels = [];
+let Workflow = new Channel("Workflow");
+channels.push(Workflow);
+
 let yourself = new User("Yo");
+let teammate2 = new User("Teammate 2");
+let teammate3 = new User("Teammate 3");
+let teammate4 = new User("Teammate 4");
+let teammate5 = new User("Teammate 5");
+let teammate6 = new User("Teammate 6");
+users.push(yourself,teammate2,teammate3,teammate4,teammate5,teammate6);
+
+Workflow.setMessage(teammate2.name, Workflow.name, "Hola Chicos!", actualDate());
+Workflow.setMessage(teammate3.name, Workflow.name, "Hola, encantado.", actualDate());
+Workflow.setMessage(teammate4.name, Workflow.name, "¿Cómo estáis hoy? Tenemos bastante trabajo.", actualDate());
+Workflow.setMessage(teammate5.name, Workflow.name, "Yo he terminado mi parte, ahora hablo con Alberto para ayudarle con lo suyo", actualDate());
+Workflow.setMessage(teammate6.name, Workflow.name, "Recibido, te mando un mensaje directo cuando pueda y te comento lo que estoy haciendo. Me viene bien un poco de ayuda jejejeje", actualDate());
 
 $("#add-user").click(function (){
     let new_name = prompt("Mandarle un mensaje directo a: ");
@@ -34,56 +70,104 @@ $("#add-user").click(function (){
 });
 
 
-function enterChat(name){
-    $("#ipt-new-msg").focus();
+function enterChat(name, isChannel){
     let to = name;
+
+    $("#ipt-new-msg").focus();
     $(".chat-title").html(`${to}`);
     $(".chat-descript").html(`Este es tu chat con ${to}`);
-    printMessages(to);
+    printMessages(to, isChannel);
 }
 
-function printMessages(to){
-    $(".msg-screen").html("");
+function printMessages(to, isChannel){
+    if(isChannel){
+        function getInfo(){
+            for (let index = 0; index < channels.length; index++) {
+                if(channels[index].name === to){
+                    return channels[index];
+                }
+            }
+        }
+        let info = getInfo();
+        console.log(info.name);
 
-    let res = yourself.getMessage(yourself.name, to);
-    if(res.length == 0){
-        let today = new Date();
-        let dd = String(today.getDate()).padStart(2, '0');
-        let mm = String(today.getMonth() + 1).padStart(2, '0');
-        let yyyy = today.getFullYear();
-        today = dd + '/' + mm + '/' + yyyy;
+        //Sí que es un canal.
+        $(".msg-screen").html("");
+        let today = actualDate();
+        let res = info.getMessage(info.name);
 
-        $(".msg-screen").append(`
-        <div class="msg"> 
-        <div class="flex-msg">
-            <img id="profile-msg" src="img/profile.jpg" alt="foto de perfil">
-            <div class="details-msg">
-                    <div class="flex-date-msg">
-                        <p id="username-msg">Oops!</p>
-                        <span id="date-msg">${today}</span>
-                    </div>
-                <span id="content-msg">No tienes ningún mensaje con ${to}. ¡Dile Hola!</span>
+        if(res.length == 0){
+            $(".msg-screen").append(`
+            <div class="msg"> 
+            <div class="flex-msg">
+                <img id="profile-msg" src="img/profile.jpg" alt="foto de perfil">
+                <div class="details-msg">
+                        <div class="flex-date-msg">
+                            <p id="username-msg">Oops!</p>
+                            <span id="date-msg">${today}</span>
+                        </div>
+                    <span id="content-msg">No tienes ningún mensaje con ${info.name}. ¡Dile Hola!</span>
+                </div>
             </div>
-        </div>
-        </div>
-        `);
+            </div>
+            `);
+        }
+        res.forEach(msg => {
+            $(".msg-screen").append(`
+            <div class="msg"> 
+            <div class="flex-msg">
+                <img id="profile-msg" src="img/profile.jpg" alt="foto de perfil">
+                <div class="details-msg">
+                        <div class="flex-date-msg">
+                            <p id="username-msg">${msg.from}</p>
+                            <span id="date-msg">${msg.date}</span>
+                        </div>
+                    <span id="content-msg">${msg.content}</span>
+                </div>
+            </div>
+            </div>
+            `);
+        });
+
+    }else{
+      //No es un canal.
+      $(".msg-screen").html("");
+      let today = actualDate();
+      let res = yourself.getMessage(yourself.name, to);
+      if(res.length == 0){
+          $(".msg-screen").append(`
+          <div class="msg"> 
+          <div class="flex-msg">
+              <img id="profile-msg" src="img/profile.jpg" alt="foto de perfil">
+              <div class="details-msg">
+                      <div class="flex-date-msg">
+                          <p id="username-msg">Oops!</p>
+                          <span id="date-msg">${today}</span>
+                      </div>
+                  <span id="content-msg">No tienes ningún mensaje con ${to}. ¡Dile Hola!</span>
+              </div>
+          </div>
+          </div>
+          `);
+      }
+      res.forEach(msg => {
+          $(".msg-screen").append(`
+          <div class="msg"> 
+          <div class="flex-msg">
+              <img id="profile-msg" src="img/profile.jpg" alt="foto de perfil">
+              <div class="details-msg">
+                      <div class="flex-date-msg">
+                          <p id="username-msg">${msg.from}</p>
+                          <span id="date-msg">${msg.date}</span>
+                      </div>
+                  <span id="content-msg">${msg.content}</span>
+              </div>
+          </div>
+          </div>
+          `);
+      });  
     }
-    res.forEach(msg => {
-        $(".msg-screen").append(`
-        <div class="msg"> 
-        <div class="flex-msg">
-            <img id="profile-msg" src="img/profile.jpg" alt="foto de perfil">
-            <div class="details-msg">
-                    <div class="flex-date-msg">
-                        <p id="username-msg">${msg.from}</p>
-                        <span id="date-msg">${msg.date}</span>
-                    </div>
-                <span id="content-msg">${msg.content}</span>
-            </div>
-        </div>
-        </div>
-        `);
-    });
+    
 }
 
 $("#ipt-new-msg").on('keypress',function(event) {
@@ -92,16 +176,11 @@ $("#ipt-new-msg").on('keypress',function(event) {
         sendMessage(to);
     }
 });
-  
 
 function sendMessage(destinatary){
     let to = destinatary;
-    let today = new Date();
-    let dd = String(today.getDate()).padStart(2, '0');
-    let mm = String(today.getMonth() + 1).padStart(2, '0');
-    let yyyy = today.getFullYear();
+    let today = actualDate();
 
-    today = dd + '/' + mm + '/' + yyyy;
     let content = $("#ipt-new-msg").val();
     yourself.setMessage(yourself.name, to, content, today);
     printMessages(to);
@@ -109,14 +188,25 @@ function sendMessage(destinatary){
     $("#ipt-new-msg").val("");
 }
 
-    
-// //AÑADIR NUEVO CANAL A LA LISTA
-// $("#add-channel").click(func = () => {
-//     let name_new_channel = prompt("nombre del canal nuevo");
-//     if(name_new_channel != null && name_new_channel != ""){
-//         channels.push(name_new_channel);
-//         $("#channel_list").append(`<p># ${name_new_channel}</p>`);
-//     }else{
-//         alert("Información no válida");
-//     }
-// });
+$("#add-channel").click(function (){
+    let new_channel = prompt("Introduce el nombre del nuevo canal");
+
+    if(new_channel != null && new_channel != ""){
+        const channel = new Channel(new_channel);
+        channels.push(channel);
+        $("#channel_list").append(`<p onclick="enterChat('${new_channel}', true)"><i class="fa-solid fa-chat"></i> ${new_channel}</p>`);
+        
+    }else{
+        alert("Información no válida");
+    }
+});
+
+function actualDate(){
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0');
+    let yyyy = today.getFullYear();
+    today = dd + '/' + mm + '/' + yyyy;
+
+    return today;
+}
